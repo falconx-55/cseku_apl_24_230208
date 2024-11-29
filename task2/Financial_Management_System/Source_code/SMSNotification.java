2e76b921e24ec1850db441a9bf475b32ae8416ad
Md Mahmudul Hasan Emon <90237014+falconx-55@users.noreply.github.com>
2024-11-29 22:55:30 +0600

    Create SMSNotification.java

---
+++ task2/Financial_Management_System/Source_code/SMSNotification.java
@@ -0,0 +1,17 @@
+public class SMSNotification {
+    public void sendSMS(String phoneNumber, String message) {
+      
+    }
+
+    public void notifyTransfer(String customerName, double amount, Account fromAccount, Account toAccount) {
+       
+    }
+
+    public void notifyLoanApproval(String customerName, double loanAmount) {
+       
+    }
+
+    public void notifyBalanceUpdate(String customerName, double balance) {
+        
+    }
+}
