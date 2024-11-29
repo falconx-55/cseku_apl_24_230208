fbb2899fafe173937fca3689b2c0a14fd97660c0
Md Mahmudul Hasan Emon <90237014+falconx-55@users.noreply.github.com>
2024-11-29 22:50:50 +0600

    Create BankActivity.java

---
+++ task2/Financial_Management_System/Source_code/BankActivity.java
@@ -0,0 +1,19 @@
+public class BankActivity {
+    private SMSNotification smsNotification;
+
+    public BankActivity() {
+       
+    }
+
+    public void transferMoney(Account fromAccount, Account toAccount, double amount, Customer customer) {
+       
+    }
+
+    public void approveLoan(Customer customer, Loan loan) {
+       
+    }
+
+    public void assistCustomer(Employee employee, Customer customer) {
+        
+    }
+}
