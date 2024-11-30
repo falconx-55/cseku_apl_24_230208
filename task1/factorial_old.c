7d1c8471f5e5e1cd515e9af5e2eeb0f2e305df35
Md Mahmudul Hasan Emon <90237014+falconx-55@users.noreply.github.com>
2024-11-30 10:40:16 +0600

    Create factorial_old.c

---
+++ task1/factorial_old.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int factorial(int num)
+{
+    if (num == 0)
+        return 1;
+    return num * factorial(num - 1);
+}
+
+int main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Factorial of %d is %d\n", num, factorial(num));
+
+    return 0;
+}
