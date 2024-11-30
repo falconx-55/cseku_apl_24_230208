945d77a7066be4277ffcb5d8f64167a6b43410bb
Md Mahmudul Hasan Emon <90237014+falconx-55@users.noreply.github.com>
2024-11-30 10:04:22 +0600

    Create BankController.java

---
+++ task3/CONTROLLER/BankController.java
@@ -0,0 +1,28 @@
+package CONTROLLER;
+import MODEL.Account;
+import MODEL.Customer;
+import MODEL.Deposit;
+import MODEL.Withdraw;
+import VIEW.BankView;
+public class BankController {
+    private BankView view;
+    private Customer customer;
+    private Account account;
+    private Deposit deposit;
+    private Withdraw withdraw;
+
+    public BankController(BankView view, Customer customer, Account account) {
+    }   // constructor
+
+    public void displayCustomerInfo() {
+    }
+
+    public void depositMoney(double amount) {
+    }
+
+    public void withdrawMoney(double amount) {
+    }
+
+    public void displayBalance() {
+    }
+}
