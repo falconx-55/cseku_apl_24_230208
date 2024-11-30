f18d3824026f2800c6fff297f92f85aa9ab94e13
Md Mahmudul Hasan Emon <90237014+falconx-55@users.noreply.github.com>
2024-11-30 10:31:41 +0600

    Create BankView.java

---
+++ task3/VIEW/BankView.java
@@ -0,0 +1,19 @@
+package VIEW;
+
+public class BankView {
+
+    public void displayMessage(String message) {
+    }
+    // display current balance of
+    public void displayBalance(double balance) {
+    }
+
+    public void displayCustomerInfo(String name, String address) {
+    }
+
+    public void displayTransactionSuccess(String message) {
+    }
+
+    public void displayTransactionFailure(String message) {
+    }
+}
